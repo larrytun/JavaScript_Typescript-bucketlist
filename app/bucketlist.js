c8a8421288cac1abb4c77cac1c801e5ec08c36f3
Larry Tunseth and Laura Krouse <larry+laura@>
2016-10-17 11:26:08 -0700

    add child interfaces, add parent interfaces, add constructor objects defining who is assigned to tasks

--- app/bucketlist.js
+++ app/bucketlist.js
@@ -4,9 +4,10 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 var Task = (function () {
-    function Task(description, priority) {
+    function Task(description, priority, assignedTo) {
         this.description = description;
         this.priority = priority;
+        this.assignedTo = assignedTo;
         this.done = false;
     }
     Task.prototype.markDone = function () {
@@ -16,35 +17,56 @@ var Task = (function () {
 }());
 var ExploreTask = (function (_super) {
     __extends(ExploreTask, _super);
-    function ExploreTask() {
-        _super.apply(this, arguments);
+    function ExploreTask(description, priority, assignedTo) {
+        _super.call(this, description, priority, assignedTo);
+        this.description = description;
+        this.priority = priority;
+        this.assignedTo = assignedTo;
     }
     return ExploreTask;
 }(Task));
 var MeetTask = (function (_super) {
     __extends(MeetTask, _super);
-    function MeetTask(description, priority) {
+    function MeetTask(description, priority, assignedTo) {
         _super.call(this, description, priority);
         this.description = description;
         this.priority = priority;
+        this.assignedTo = assignedTo;
     }
     return MeetTask;
 }(Task));
+var HobbyTask = (function (_super) {
+    __extends(HobbyTask, _super);
+    function HobbyTask(description) {
+        _super.call(this, description, "low");
+        this.description = description;
+    }
+    return HobbyTask;
+}(Task));
+var playa = {
+    name: "Playa Play",
+    email: "playaplay@gmail.com"
+};
+var ken = {
+    name: "Ken Bone",
+    email: "reddit.com"
+};
+var wanda = {
+    name: "wanda what",
+    email: "ww.com"
+};
 var tasks = [];
-tasks.push(new ExploreTask("Climb Everest", "high"));
-tasks.push(new ExploreTask("Go to Sweden", "high"));
-tasks.push(new ExploreTask("Go to Mars", "low"));
-tasks[0].markDone();
-console.log(tasks);
-// var tasks: Task[] =[];
-tasks.push(new MeetTask("Meet the Obama's", "high"));
-tasks.push(new MeetTask("Meet Bernie Sanders", "high"));
-tasks.push(new MeetTask("Meet the Dali Lama", "low"));
-tasks[0].markDone();
-console.log(tasks);
+tasks.push(new ExploreTask("Climb Everest", "High", playa));
+tasks.push(new ExploreTask("Go to Sweden", "High", ken));
+tasks.push(new ExploreTask("Go to Mars", "Low", wanda));
+tasks.push(new HobbyTask("Practice CODING"));
+tasks.push(new HobbyTask("Bake a pie."));
 var today = new Date();
 var tomorrow = today;
 tomorrow.setDate(today.getDate() + 1);
 var nextDay = today;
 nextDay.setDate(today.getDate() + 2);
+tasks.push(new MeetTask("Meet the Obama's", "High"));
+tasks.push(new MeetTask("Meet Bernie Sanders", "High"));
+tasks.push(new MeetTask("Meet the Dali Lama", "Low"));
 console.log(tasks);
--- app/bucketlist.ts
+++ app/bucketlist.ts
@@ -1,35 +1,69 @@
-class Task {
+
+interface IPerson {
+  name: string;
+  email: string;
+}
+
+interface ITask {
+  description: string;
+  done: boolean;
+  priority: string;
+  markDone(): void;
+  assignedTo?: IPerson;
+}
+
+
+
+class Task implements ITask {
   done: boolean = false;
-  constructor(public description: string, public priority: string){}
+  constructor(public description: string, public priority: string, public assignedTo?: IPerson){}
   markDone(){
     this.done = true;
   }
 }
-class ExploreTask extends Task {}
+class ExploreTask extends Task {
+  constructor(public description: string, public priority: string, public assignedTo: IPerson){
+  super(description, priority, assignedTo);
+  }
+}
 
 class MeetTask extends Task {
-  constructor(public description: string, public priority: string){
+  constructor(public description: string, public priority: string, public assignedTo?: IPerson){
     super(description, priority);
   }
 }
 
+class HobbyTask extends Task {
+  constructor(public description: string){
+    super(description, "low");
+  }
+}
+
+var playa: IPerson = {
+  name: "Playa Play",
+  email: "playaplay@gmail.com"
+}
+
+var ken: IPerson = {
+  name: "Ken Bone",
+  email: "reddit.com"
+}
+
+var wanda: IPerson = {
+  name: "wanda what",
+  email: "ww.com"
+}
 
 
+var tasks = [];
 
-var tasks: Task[] =[];
-tasks.push(new ExploreTask("Climb Everest", "high"));
-tasks.push(new ExploreTask("Go to Sweden", "high"));
-tasks.push(new ExploreTask("Go to Mars", "low"));
-tasks[0].markDone();
-console.log(tasks);
+tasks.push(new ExploreTask("Climb Everest", "High", playa));
+tasks.push(new ExploreTask("Go to Sweden", "High", ken));
+tasks.push(new ExploreTask("Go to Mars", "Low", wanda));
 
+tasks.push(new HobbyTask("Practice CODING"));
+tasks.push(new HobbyTask("Bake a pie."));
 
-// var tasks: Task[] =[];
-tasks.push(new MeetTask("Meet the Obama's", "high"));
-tasks.push(new MeetTask("Meet Bernie Sanders", "high"));
-tasks.push(new MeetTask("Meet the Dali Lama", "low"));
-tasks[0].markDone();
-console.log(tasks);
 
 var today = new Date();
 var tomorrow = today;
@@ -37,4 +71,9 @@ var tomorrow = today;
 tomorrow.setDate(today.getDate() + 1);
 var nextDay = today;
 nextDay.setDate(today.getDate() + 2);
+
+tasks.push(new MeetTask("Meet the Obama's", "High"));
+tasks.push(new MeetTask("Meet Bernie Sanders", "High"));
+tasks.push(new MeetTask("Meet the Dali Lama", "Low"));
+
 console.log(tasks);
